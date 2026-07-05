caff0816caf0e5ad72f8a194ad9d48c5dd996069
telegram137 <allfakesmile000@gmail.com>
2026-07-05 09:36:33 +0600

    Update config.py with hardcoded credentials
    Replaced environment variable values with hardcoded tokens.

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SMSBOWER_API_KEY = os.getenv("SMSBOWER_API_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = os.getenv("8629866531:AAFvBUiVxmDKEVGVifa2i9UR3QBMqHmtU2Q")
+SMSBOWER_API_KEY = os.getenv("5VKQe0tU9J7JvccBr0fCB3PbVHGiq7by")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6715426106"))
