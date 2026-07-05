d66229c308a0c3f2ad49b5c965472f2da8cc749a
telegram137 <allfakesmile000@gmail.com>
2026-07-05 09:47:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BOT_TOKEN = os.getenv("8629866531:AAFvBUiVxmDKEVGVifa2i9UR3QBMqHmtU2Q")
 SMSBOWER_API_KEY = os.getenv("5VKQe0tU9J7JvccBr0fCB3PbVHGiq7by")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "6715426106"))
+ADMIN_ID = 6715426106
